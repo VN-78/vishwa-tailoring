c7fb4385cab80514daf1d6ad4cc073a66a922533
VN-78 <vishwanath.s.god@gmail.com>
2026-05-26 09:15:36 +0530

    pattern icons svg

---
+++ src/lib/components/atoms/icons/TapeMeasure.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+	let { size = 16, class: className = "" } = $props();
+</script>
+
+<svg
+	id="Tape-Measure--Streamline-Atlas"
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="-0.5 -0.5 16 16"
+	class={className}
+	height={size}
+	width={size}
+>
+	<desc>Tape Measure Streamline Icon: https://streamlinehq.com</desc>
+	<defs></defs>
+	<path
+		d="M9.2875 9.2875a4.175 4.175 0 1 1 -8.35 0A4.08125 4.08125 0 0 1 1.34375 7.5a4.16875 4.16875 0 0 1 7.5375000000000005 0 4.08125 4.08125 0 0 1 0.40625 1.7874999999999999Z"
+		fill="none"
+		stroke="currentColor"
+		stroke-miterlimit="10"
+		stroke-width="1"
+	></path>
+	<path
+		d="M6.90625 9.2875a1.7937500000000002 1.7937500000000002 0 1 1 -3.5812500000000003 0 1.73125 1.73125 0 0 1 0.17500000000000002 -0.7625 1.7874999999999999 1.7874999999999999 0 0 1 3.23125 0 1.73125 1.73125 0 0 1 0.17500000000000002 0.7625Z"
+		fill="none"
+		stroke="currentColor"
+		stroke-miterlimit="10"
+		stroke-width="1"
+	></path>
+	<path
+		d="M14.0625 1.53125v3.5812500000000003H5.1125a4.1625 4.1625 0 0 0 -3.75 2.3874999999999997 4.08125 4.08125 0 0 0 -0.40625 1.7874999999999999V5.7125a3.125 3.125 0 0 1 0.05 -0.625 4.1625 4.1625 0 0 1 4.125 -3.5812500000000003Z"
+		fill="none"
+		stroke="currentColor"
+		stroke-miterlimit="10"
+		stroke-width="1"
+	></path>
+	<path
+		d="m11.675 1.53125 0 2.3874999999999997"
+		fill="none"
+		stroke="currentColor"
+		stroke-miterlimit="10"
+		stroke-width="1"
+	></path>
+	<path
+		d="m9.2875 1.53125 0 1.7937500000000002"
+		fill="none"
+		stroke="currentColor"
+		stroke-miterlimit="10"
+		stroke-width="1"
+	></path>
+	<path
+		d="m6.90625 1.53125 0 2.3874999999999997"
+		fill="none"
+		stroke="currentColor"
+		stroke-miterlimit="10"
+		stroke-width="1"
+	></path>
+	<path
+		d="m4.518750000000001 1.53125 0 1.7937500000000002"
+		fill="none"
+		stroke="currentColor"
+		stroke-miterlimit="10"
+		stroke-width="1"
+	></path>
+</svg>
