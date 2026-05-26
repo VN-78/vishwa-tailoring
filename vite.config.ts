66dd07250b5af9d26f5d583db18773bf109489b4
VN-78 <vishwanath.s.god@gmail.com>
2026-05-27 03:24:29 +0530

    added paraglide

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
+import { paraglide } from '@inlang/paraglide-sveltekit/vite'
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [tailwindcss(), sveltekit()] });
+export default defineConfig({ plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),tailwindcss(), sveltekit()] });
