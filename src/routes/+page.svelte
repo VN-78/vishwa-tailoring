2a99cba1cff39a0d576ac603853120524cbfd9a2
VN-78 <vishwanath.s.god@gmail.com>
2026-05-27 07:01:56 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,10 +20,7 @@
 	<div in:fly={{ y: -40, duration: 600, easing: quartOut }}>
 		<Header />
 	</div>
-
-	<main
-		class="min-h-screen bg-background px-4 pt-14 pb-2 text-foreground md:px-8 md:pt-24 md:pb-6"
-	>
+	<main class="min-h-screen px-4 pt-14 pb-2 text-foreground md:px-8 md:pt-24 md:pb-6">
 		<div
 			in:fade={{ delay: 600, duration: 800, easing: quartOut }}
 			class="max-w-8xl mx-auto w-full md:px-24 lg:px-32"
@@ -33,10 +30,10 @@
 			<Services />
 			<Gallery />
 			<Location />
-            
-            <footer class="border-t border-border/10 py-12 text-center text-muted-foreground">
-                <p>© {new Date().getFullYear()} Vishwa Tailoring. All rights reserved.</p>
-            </footer>
+
+			<footer class="border-t border-border/10 py-12 text-center text-muted-foreground">
+				<p>© {new Date().getFullYear()} Vishwa Tailoring. All rights reserved.</p>
+			</footer>
 		</div>
 	</main>
 {/if}
