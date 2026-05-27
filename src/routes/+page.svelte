12093e25ceedf89a058a41578feb9ca7b1324eea
VN-78 <vishwanath.s.god@gmail.com>
2026-05-27 05:37:35 +0530

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import Header from '$lib/components/organisms/Header.svelte';
-	import BackgroundPattern from '$lib/components/molecules/BackgroundPattern.svelte';
-	import { fly } from 'svelte/transition';
+	import Hero from '$lib/components/organisms/Hero.svelte';
+	import About from '$lib/components/organisms/About.svelte';
+	import Services from '$lib/components/organisms/Services.svelte';
+	import Gallery from '$lib/components/organisms/Gallery.svelte';
+	import Location from '$lib/components/organisms/Location.svelte';
+	import { fade, fly } from 'svelte/transition';
 	import { quartOut } from 'svelte/easing';
 	import { onMount } from 'svelte';
 
@@ -12,27 +16,27 @@
 	});
 </script>
 
-<BackgroundPattern />
-
-<!-- Change header accordingly to project  -->
-<svelte:head>
-	<title>Svelte Template</title>
-
-	<meta
-		name="description"
-		content="SvelteKit template used for quick Scafolding of new projects."
-	/>
-
-	<meta property="og:title" content="svelte-template" />
-	<meta
-		property="og:description"
-		content="svelte-template is a SvelteKit template used for quick Scafolding of new projects."
-	/>
-	<meta property="og:type" content="website" />
-</svelte:head>
-
 {#if isMounted}
 	<div in:fly={{ y: -40, duration: 600, easing: quartOut }}>
 		<Header />
 	</div>
+
+	<main
+		class="min-h-screen bg-background px-4 pt-14 pb-2 text-foreground md:px-8 md:pt-24 md:pb-6"
+	>
+		<div
+			in:fade={{ delay: 600, duration: 800, easing: quartOut }}
+			class="max-w-8xl mx-auto w-full md:px-24 lg:px-32"
+		>
+			<Hero />
+			<About />
+			<Services />
+			<Gallery />
+			<Location />
+            
+            <footer class="border-t border-border/10 py-12 text-center text-muted-foreground">
+                <p>© {new Date().getFullYear()} Vishwa Tailoring. All rights reserved.</p>
+            </footer>
+		</div>
+	</main>
 {/if}
