db4c02c34decb2668ca862f6eb0db659d8fcd4f0
VN-78 <vishwanath.s.god@gmail.com>
2026-05-27 05:37:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { paraglide } from '@inlang/paraglide-sveltekit/vite'
+import { paraglideVitePlugin } from '@inlang/paraglide-js'
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig({ plugins: [paraglide({ project: './project.inlang', outdir: './src/lib/paraglide' }),tailwindcss(), sveltekit()] });
+export default defineConfig({ plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide', strategy: ['url', 'cookie', 'baseLocale'], emitTsDeclarations: true }),tailwindcss(), sveltekit()] });
