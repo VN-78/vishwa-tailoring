9c1f4d69e5ce4cf02ae1526eb0cbabc05020b87a
VN-78 <vishwanath.s.god@gmail.com>
2026-05-27 05:37:42 +0530

    Create index.ts

---
+++ src/lib/components/atoms/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as Button } from './Button.svelte';
+export { default as Needle } from './Needle.svelte';
+export { default as Pin } from './Pin.svelte';
+export { default as TapeMeasure } from './TapeMeasure.svelte';
